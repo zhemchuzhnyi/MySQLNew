32025e1ee11739f61f3cafa4cf2825bd202b07df
Otus_PC <a_reshkin@inbox.ru>
2025-03-26 00:38:19 +0300

    Update

---
+++ src/main/java/dao/AbsTable.java
@@ -0,0 +1,38 @@
+package dao;
+
+import utils.ConnectionManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbsTable {
+    protected String tableName;
+    protected Map<String, String> columns = new HashMap<>();
+
+    public AbsTable(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public void create() {
+        String sqlRequest = String.format("CREATE TABLE IF NOT EXISTS %s (%s)", this.tableName, convertMapColumnsToString());
+        System.out.println(sqlRequest);
+        ConnectionManager.ExecuteQuery(sqlRequest);
+//
+//        try {
+//            ConnectionManager.getInstance().executeQuery(sqlRequest);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    private String convertMapColumnsToString() {
+        String result = "";
+        for(Map.Entry<String, String> el : columns.entrySet()){
+            result += el.getKey() + " " + el.getValue() + ",";
+        }
+        result = result.substring(0, result.length()-1);
+        return result;
+    }
+}
--- src/main/java/utils/ConnectionManager.java
+++ src/main/java/utils/ConnectionManager.java
@@ -1,17 +1,29 @@
 package utils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnectionManager {
         private static final String url = "jdbc:mysql://sql.home.kartushin.su:3306/test";
         private static final String user = "admin";
         private static final String password = "711267";
 
-        private ConnectionManager() {}
+        private ConnectionManager() {
+        }
 
         public static Connection getConnection() throws SQLException {
-            return DriverManager.getConnection(url, user, password);
+                return DriverManager.getConnection(url, user, password);
+        }
+
+        public static ResultSet ExecuteQuery(String query) {
+                try (Connection conn = getConnection()) {
+                        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+                                return rs;
+                        }
+
+                } catch (Exception e) {
+                        System.out.println("error");
+                }
+
+            return null;
         }
 }
